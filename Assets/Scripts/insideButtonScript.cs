71d145d1cec9afa0bf227f8633af18840b8b24fc
Avada Kedavra! <1499449418@qq.com>
2019-05-12 19:03:21 +0800

    update script for reading.

---
+++ Assets/Scripts/insideButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class insideButtonScript : MonoBehaviour
+{
+    public GameObject elevator;
+    public elevatorScript elevatorS;
+    public ArrayList floor_wait = new ArrayList();
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevatorS = elevator.GetComponent<elevatorScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FLOOR(int i)
+    {
+        elevator.SendMessage("AddTasks", i);
+    }
+}
